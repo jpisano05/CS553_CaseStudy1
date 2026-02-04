7c37abbf4528a89a6929b62308259f682494d1f4
jpisano05 <jjpisano@wpi.edu>
2026-02-04 16:48:19 -0500

    added text boxes

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def respond(
 
 
 gr.Markdown("The Coffee Connoisseur")
-gr.Markdown("Enter a taste profile for a desired coffee drink and the Coffee Connoisseur will recommend you a drink.")
-gr.Markdown("For best results, keep inputs short like \"Floral and Delicate\" or \"Chocolatey and nutty\"")
+gr.Textbox(value = "Enter a taste profile for a desired coffee drink and the Coffee Connoisseur will recommend you a drink.")
+gr.Textbox(value = "For best results, keep inputs short like \"Floral and Delicate\" or \"Chocolatey and nutty\"")
 
 chatbot = gr.ChatInterface(
     title="The Coffee Connoisseur",
