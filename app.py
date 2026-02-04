b1f547e6c0b8021c9ea968278c2a4ebedbbd4706
jpisano05 <jjpisano@wpi.edu>
2026-02-04 16:41:39 -0500

    reworked UI

--- app.py
+++ app.py
@@ -81,7 +81,6 @@ chatbot = gr.ChatInterface(
     title="The Coffee Connoisseur",
     fn=respond,
     additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Checkbox(label="Use Local Model?", value = False),
     ],
