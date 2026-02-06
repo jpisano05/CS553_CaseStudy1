7c7b5be1da41b1583b34bfaa26b81e7d4507d1ea
jpisano05 <jjpisano@wpi.edu>
2026-02-05 19:01:48 -0500

    updated test.yml

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,8 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import huggingface_hub
-import transformers
 import app
 
 def test_api_requires_token():
